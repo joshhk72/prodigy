fda487ff4d3106c6b9dd7e4ff3f32d39606cc2bc
Han Kim <joshhk72@gmail.com>
2019-10-17 14:09:26 -0400

    Fix bug where canceling annotations didn't change the url

--- frontend/components/track_show/second_column/annotation_form.jsx
+++ frontend/components/track_show/second_column/annotation_form.jsx
@@ -14,6 +14,7 @@ class AnnotationForm extends React.Component {
     e.preventDefault();
     this.props.removeAnnotation(this.state.id);
     this.props.destroyForm(this.state.id);
+    this.props.history.push(`/tracks/${this.props.match.params.trackId}`);
   }
 
   handleSave(e) {
