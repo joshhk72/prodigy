6ce5ed1a2bb7d2f7b2841cbad1172bdfd6b75268
Han Kim <joshhk72@gmail.com>
2019-12-04 03:48:08 -0500

    Update formatting for annotations form

--- frontend/components/track_show/questions/answer_body.jsx
+++ frontend/components/track_show/questions/answer_body.jsx
@@ -7,11 +7,6 @@ const AnswerBody = props => {
   const converter = new showdown.Converter({ extensions: ['youtube'] });
   const html = converter.makeHtml(answer.body);
   // don't need to sanitize html because it's only on the client side
-  console.log(html);
-  // const split = answer.body.split(/\r?\n/);
-  // const parsed = split.map((line, i) => {
-  //   return <p key={String(answer.id) + '-' + String(i)}>{line}</p>
-  // })
   return (
     <div className="answer-body" dangerouslySetInnerHTML={{__html: html}}>
     </div>
